dc33bdf70f81d2636f7de581c2dc2bee0f58f62a
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-24 23:58:15 +0900

    useReducer로 input상태 관리하기

--- chapter08_hooks/src/App.js
+++ chapter08_hooks/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import Counter from './Counter';
+import Info from './Info';
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => {setVisible(!visible);}}>{visible? '숨기기':'보이기'}</button>
-      {visible && <Counter />}
+      {visible && <Info />}
     </div>
   );
 }
