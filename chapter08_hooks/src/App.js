f3c5ae195dd34a11c7958cff39e24b72f0cec89f
kim-hyunjin <hyunjin1612@gmail.com>
2020-09-24 23:39:38 +0900

    useEffect 뒷정리 함수

--- chapter08_hooks/src/App.js
+++ chapter08_hooks/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Info from './Info';
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="App">
-      <Info />
+      <button onClick={() => {setVisible(!visible);}}>{visible? '숨기기':'보이기'}</button>
+      {visible && <Info />}
     </div>
   );
 }
