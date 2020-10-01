6b959d71be06273478a075b6106bc10a36317d27
kim-hyunjin <hyunjin1612@gmail.com>
2020-10-01 18:41:19 +0900

    Link 컴포넌트 사용

--- 13_spa/src/App.js
+++ 13_spa/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} exact={true} />
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
