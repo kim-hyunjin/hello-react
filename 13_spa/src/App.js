14924378b8187b278e9f9110515d445826c473d3
kim-hyunjin <hyunjin1612@gmail.com>
2020-10-01 19:19:31 +0900

    서브라우트

--- 13_spa/src/App.js
+++ 13_spa/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -14,10 +14,13 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
