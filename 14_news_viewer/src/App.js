883cdba42c6c800b989787c0c77cd41634f654b7
kim-hyunjin <hyunjin1612@gmail.com>
2020-10-02 14:05:36 +0900

    뉴스 뷰어 UI

--- 14_news_viewer/src/App.js
+++ 14_news_viewer/src/App.js
@@ -1,26 +1,12 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=kr&' +
-          'apiKey=80c7785acfbd48448a82e895b4629224';
-  const onClick = async () => {
-    try {
-      const response = await axios.get(url);
-      setData(response.data);
-    } catch(e) {
-      console.log(e);
-    }
-  };
+
 
   return (
     <div className="App">
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+      <NewsList />
     </div>
   );
 }
---
+++ 14_news_viewer/src/components/NewsList.js
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const url = 'http://newsapi.org/v2/top-headlines?' +
+      'country=kr&' +
+      'apiKey=80c7785acfbd48448a82e895b4629224';
+      try {
+        const response = await axios.get(url);
+        setArticles(response.data.articles);
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if(loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>
+  }
+
+  if(!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map(article => (
+      <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+
+  );
+};
+
+export default NewsList;
