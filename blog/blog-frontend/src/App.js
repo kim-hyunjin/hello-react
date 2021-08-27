4e35be1de34943b551596ce2a1f3cb1136c2773e
kim-hyunjin <hyunjin1612@gmail.com>
2021-08-27 17:50:54 +0900

    페이지 정의 및 라우터 설정

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
+// /@:username => http://localhost:3000/@velopert 같은 경로에서 velopert 를 username 파라미터로 읽을 수 있게 해준다. Medium, 브런치 같은 서비스에서도 계정명을 주소 경로 안에 넣을 때 @를 넣는 방식을 사용한다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
   );
 }
 
