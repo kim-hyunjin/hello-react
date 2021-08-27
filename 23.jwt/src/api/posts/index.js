2d01128acbe68d8a76db3339e92f27cc88b74632
kim-hyunjin <hyunjin1612@gmail.com>
2021-08-27 14:21:39 +0900

    로그인 확인 미들웨어 추가

--- 23.jwt/src/api/posts/index.js
+++ 23.jwt/src/api/posts/index.js
@@ -1,14 +1,15 @@
 import Router from 'koa-router';
 import * as postCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 posts.get('/', postCtrl.list);
-posts.post('/', postCtrl.write);
+posts.post('/', checkLoggedIn, postCtrl.write);
 
 const post = new Router();
 post.get('/', postCtrl.read);
-post.delete('/', postCtrl.remove);
-post.patch('/', postCtrl.update);
+post.delete('/', checkLoggedIn, postCtrl.remove);
+post.patch('/', checkLoggedIn, postCtrl.update);
 posts.use('/:id', postCtrl.checkObjectId, post.routes()); // id 파라미터 검증 미들웨어 적용
 
 export default posts;
