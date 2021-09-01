89404ad9c5cbf827de26b60532550577182466a0
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-01 20:48:24 +0900

    title 메타데이터 정의

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -5,11 +5,15 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 // /@:username => http://localhost:3000/@velopert 같은 경로에서 velopert 를 username 파라미터로 읽을 수 있게 해준다. Medium, 브런치 같은 서비스에서도 계정명을 주소 경로 안에 넣을 때 @를 넣는 방식을 사용한다.
 function App() {
   return (
     <>
+      <Helmet>
+        <title>REACTERS</title>
+      </Helmet>
       <Route component={PostListPage} path={['/@:username', '/']} exact />
       <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
