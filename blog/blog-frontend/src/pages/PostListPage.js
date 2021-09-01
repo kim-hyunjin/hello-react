17f68b45aa726d30636e3ba43823afaf5962eb4f
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-01 17:36:27 +0900

    페이지네이션 컨테이너, 컴포넌트 정의

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
@@ -7,6 +8,7 @@ const PostListPage = () => {
     <div>
       <HeaderContainer />
       <PostListContainer />
+      <PaginationContainer />
     </div>
   );
 };
