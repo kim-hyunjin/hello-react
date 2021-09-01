34ddc173fcb4903ae9a620f20067cbe0c8c9fcba
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-01 21:00:43 +0900

    build 된 front를 backend server에서 제공

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -5,7 +5,10 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
-import createFakeData from './createFakeData';
+// import createFakeData from './createFakeData';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -29,6 +32,14 @@ router.use('/api', api.routes());
 
 // 라우터 적용하기 전에 bodyparser 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
