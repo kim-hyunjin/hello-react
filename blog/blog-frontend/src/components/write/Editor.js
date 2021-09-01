121c42e165cb692b2bc84e63d724402e226de61a
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-01 20:20:39 +0900

    글쓰기 화면 css 수정

--- blog/blog-frontend/src/components/write/Editor.js
+++ blog/blog-frontend/src/components/write/Editor.js
@@ -3,9 +3,8 @@ import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
-import Responsive from '../common/Responsive';
 
-const EditorBlock = styled(Responsive)`
+const EditorBlock = styled.div`
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
