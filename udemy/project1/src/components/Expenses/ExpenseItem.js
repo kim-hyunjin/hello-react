5e3cb6c0141bb5eda241d0cc73cfef6f86ebd74e
kim-hyunjin <hyunjin1612@gmail.com>
2022-03-01 14:17:03 +0900

    how component function are executed

--- udemy/project1/src/components/Expenses/ExpenseItem.js
+++ udemy/project1/src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,22 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expense }) {
-    const { title, date, amount } = expense;
+    let { title, date, amount } = expense;
+
+    function changeTitleClickHandler() {
+        console.log("Clicked!");
+        title = "Updated!";
+        console.log(title); // Updated! but... it doesn't reflected to our page... cuz our ExpenseItem() funtion doesn't called again!
+    }
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={date} />
+            <ExpenseDate date={date} /> {/** ExpenseDate() function called! */}
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={changeTitleClickHandler}>Change Title</button>
         </Card>
     );
 }
