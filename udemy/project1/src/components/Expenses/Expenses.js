47485062cdd3c4789e86348b6b8eed2e09b0e7ec
kim-hyunjin <hyunjin1612@gmail.com>
2022-03-26 17:12:10 +0900

    expense filter

--- udemy/project1/src/components/Expenses/Expenses.js
+++ udemy/project1/src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import { useState } from 'react';
 
 function Expenses({ expenses }) {
+  const [selectedYear, setSelectedYear] = useState('2022');
+
+  const filterChangeHandler = (selected) => {
+    console.log(selected);
+    setSelectedYear(selected);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler} />
       {expenses.map((expense) => (
         <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
       ))}
