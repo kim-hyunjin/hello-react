2f1ad11f5ee668cc516535c5527a5de36f9544a2
kim-hyunjin <hyunjin1612@gmail.com>
2022-04-18 18:22:01 +0900

    filtering expenses by year

--- udemy/project1/src/components/Expenses/Expenses.js
+++ udemy/project1/src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 function Expenses({ expenses }) {
   const [selectedYear, setSelectedYear] = useState('2022');
@@ -12,12 +12,25 @@ function Expenses({ expenses }) {
     setSelectedYear(selected);
   };
 
+  const filteredExpenses = useMemo(() => {
+    return expenses.filter((expense) => {
+      return expense.date.getFullYear().toString() === selectedYear;
+    });
+  }, [expenses, selectedYear]);
+
+  const expenseContent = useMemo(() => {
+    if (filteredExpenses.length === 0) {
+      return <p>No expenses found.</p>;
+    }
+    return filteredExpenses.map((expense) => (
+      <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
+    ));
+  }, [filteredExpenses]);
+
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler} />
-      {expenses.map((expense) => (
-        <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
-      ))}
+      {expenseContent}
     </Card>
   );
 }
