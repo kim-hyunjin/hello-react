45820bc7f0334c4c61c4229ec7644d59904243f8
kim-hyunjin <hyunjin1612@gmail.com>
2022-04-24 14:24:15 +0900

    fix bug

--- udemy/debugging-react/src/index.js
+++ udemy/debugging-react/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
