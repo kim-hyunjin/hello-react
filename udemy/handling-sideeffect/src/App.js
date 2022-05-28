47e0f9566a64a9039d2360732dc628a635e8200a
kim-hyunjin <hyunjin1612@gmail.com>
2022-05-28 22:28:07 +0900

    useContext - remove props chain

--- udemy/handling-sideeffect/src/App.js
+++ udemy/handling-sideeffect/src/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './context/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,8 +18,8 @@ function App() {
 
   // data fetching, storing data in storage is not directly associated with UI
   useEffect(() => {
-    const storedUserLoggedIn = localStorage.getItem("isLoggedIn");
-    if (storedUserLoggedIn === "1") {
+    const storedUserLoggedIn = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedIn === '1') {
       setIsLoggedIn(true);
     }
   }, []);
@@ -26,23 +27,23 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
+    localStorage.setItem('isLoggedIn', '0');
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
