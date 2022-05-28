0d172afc51a0d3a8f089a5831cf3d077f1e3299d
kim-hyunjin <hyunjin1612@gmail.com>
2022-05-28 22:37:45 +0900

    more dynamic context

--- udemy/handling-sideeffect/src/context/auth-context.js
+++ udemy/handling-sideeffect/src/context/auth-context.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, // dummy function for better IDE auto complition
 });
 
 export default AuthContext;
