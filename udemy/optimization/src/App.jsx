4ec769a6a23839ad60c1696df830748b0a0689ff
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-24 00:03:22 +0900

    useMemo

--- udemy/optimization/src/App.jsx
+++ udemy/optimization/src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useCallback } from 'react';
 
 import './App.css';
+import DemoList from './components/Demo/DemoList';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState();
   const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
   console.log('APP running');
 
@@ -22,12 +24,21 @@ function App() {
     setAllowToggle(true);
   }, []);
 
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+
+  // we don't have to make same array every time. so, memorize it!
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoList title={listTitle} items={listItems} />
       <DemoOutput show={showParagraph} />
       <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
