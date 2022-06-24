45fd70a5b801669efeba8841477ffeb5915fa830
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-25 00:21:35 +0900

    add custom http hook

--- udemy/custom-hooks/exam2/src/components/NewTask/NewTask.js
+++ udemy/custom-hooks/exam2/src/components/NewTask/NewTask.js
@@ -1,42 +1,45 @@
-import { useState } from 'react';
+import { useCallback } from 'react';
+import useHttp from '../../hooks/useHttp';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { onAddTask } = props;
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const { isLoading, error, sendRequest } = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  const createTask = useCallback(
+    (taskText, data) => {
       const generatedId = data.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+      onAddTask(createdTask);
+    },
+    [onAddTask]
+  );
+
+  const enterTaskHandler = useCallback(
+    async (taskText) => {
+      sendRequest(
+        {
+          url: 'https://react-http-6b4a6.firebaseio.com/tasks.json',
+          method: 'POST',
+          body: { text: taskText },
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+        /**
+         * bind() - it prepares the function on which it's called for future execution.
+         * "preconfigure" which arguments that function shuold receive.
+         *  you can also define what the this keyword should refer to inside of that function.
+         */
+        createTask.bind(null, taskText)
+      );
+    },
+    [sendRequest, createTask]
+  );
 
   return (
     <Section>
