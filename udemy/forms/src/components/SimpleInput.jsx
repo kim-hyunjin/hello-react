369d44f6aab324e89b6618d346dcbeeb82285d30
kim-hyunjin <hyunjin1612@gmail.com>
2022-06-26 02:42:00 +0900

    disable button when form invalid

--- udemy/forms/src/components/SimpleInput.jsx
+++ udemy/forms/src/components/SimpleInput.jsx
@@ -8,6 +8,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
@@ -49,7 +55,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className="error-text">Name must not empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
