e814e61e2847ae8f69e7a3b40d1d35692fc2ca72
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-05 21:09:50 +0900

    using Switch, exact for configuring routes

--- udemy/route/src/pages/Products.jsx
+++ udemy/route/src/pages/Products.jsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online Course</li>
+        <li>
+          <Link to="/products/1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/3">An Online Course</Link>
+        </li>
       </ul>
     </section>
   );
